b5d2b2e9bd3edccbe74359fd0f9888f11447ced2
Cyrese Mills <88636615+CyreseM@users.noreply.github.com>
2024-11-19 09:00:51 +0000

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,54 @@
-
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCloud } from '@fortawesome/free-solid-svg-icons'
-import './App.css'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCloud, faRotate } from "@fortawesome/free-solid-svg-icons";
+import "./App.css";
+import { useNavigate} from "react-router-dom";
+import { useState } from "react";
 
 function App() {
-
+  const API_KEY = "2b03189ba6f413149db50570d19ea8ae";
+  const [city, setCity] = useState("");
+  const [displayName, setDisplayName] = useState(null);
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
+  const navigate = useNavigate();
   return (
     <>
       <header>
-        <p><FontAwesomeIcon icon={faCloud}/> MayWeather</p>
+        <p onClick={()=>navigate('/')}>
+          <FontAwesomeIcon icon={faCloud} /> MayWeather
+        </p>
       </header>
       <div className="hero">
         <div>
           <h2>MayWeather</h2>
-          <p>Up to date weather forecasts and history brought to you in an elegant comprehensible way.</p>
-        </div>   
+          <p>
+            Up to date weather forecasts and history brought to you in an
+            elegant comprehensible way.
+          </p>
+        </div>
       </div>
-      <div className='weatherdisplay'>
+      <div className="weatherdisplay">
+        <div>
+          <div className="searchbox" placeholder="Search City">
+            <input type="search" value={city} onChange={(e)=>setCity(e.target.value)}/>
+            <button>Search</button>
+          </div>
+          <div className="metric">
+            <p><FontAwesomeIcon icon={faRotate} /></p>
+            <p>Metric: °C, m/s</p>
+            <p>Imperial: °F, mph</p>
+          </div>
+        </div>
 
+        <div className="weatherinfo">
+          <div>
+            <p>Date</p>
+            <h2>{displayName??"Accra" }</h2>
+            <p>Temperature</p>
+          </div>
+        </div>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
